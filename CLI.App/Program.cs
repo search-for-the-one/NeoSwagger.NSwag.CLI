25587594ff315305dc3ab50e6d580dd86a8fbcc6
Zach Saw <zach.saw@carsales.com.au>
2020-06-11 15:47:59 +1000

    Add CLI app

---
+++ CLI.App/Program.cs
@@ -0,0 +1,9 @@
+﻿using NeoSwagger.NSwag.CLI;
+
+namespace CLI.App
+{
+    public static class Program
+    {
+        public static void Main(string[] args) => new CommandLineInterface().Run(args);
+    }
+}
